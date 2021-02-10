cb6833936f9c0d945e5a4cf1574d4c6029790367
Romain Toublanc <romainr9@hotmail.fr>
2021-02-10 22:32:17 +0000

    limit data in dev sql macro

--- macros/limit_data_in_dev.sql
+++ macros/limit_data_in_dev.sql
@@ -1,7 +1,5 @@
-{% macro limit_data_in_dev( column_name) %}
--- {% if target.name == 'romain-dev' %}
-target.name
-blabla
--- where {{ column_name}} >= dateadd( 'day', -3, current_timestamp)
--- {% endif %}
+{% macro limit_data_in_dev( column_name, dev_days_of_data = 3) %}
+{% if target.name == 'default' %}
+where {{ column_name}} >= dateadd( 'day', - {{ dev_days_of_data}}, current_timestamp)
+{% endif %}
 {% endmacro %}
