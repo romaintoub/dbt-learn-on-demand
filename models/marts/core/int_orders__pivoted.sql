084a861400abfefe24f9d942bb610de41254fb2b
Romain Toublanc <romainr9@hotmail.fr>
2021-02-10 21:36:14 +0000

    int_orders__pivoted.sql

--- models/marts/core/int_orders__pivoted.sql
+++ models/marts/core/int_orders__pivoted.sql
@@ -1,14 +1,21 @@
+{%- set payment_methods = ['bank_transfer','credit_card','coupon','gift_card'] -%}
+ 
 with payments as (
-    select * from {{ ref('stg_payments')}}
+   select * from {{ ref('stg_payments') }}
 ),
-
-pivoted as (
-    select 
-        order_id, 
-        sum( case when payment_method = 'bank_transfer' then amount else 0 end) as bank_transfer_amount
-    from payments
-    where status = 'success'
-    group by 1
+ 
+final as (
+   select
+       order_id,
+       
+       {% for payment_method in payment_methods -%}
+ 
+       sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount
+       {%- if not loop.last -%},{% endif %}
+ 
+       {% endfor %}
+   from {{ ref('stg_payments') }}
+   group by 1
 )
-
-select * from pivoted
+ 
+select * from final
